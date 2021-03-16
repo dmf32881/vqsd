c540f7494747c8f52220b6f0ba4b637282879fe2
Dillion Fox <dillion.m.fox@gsk.com>
2021-03-16 10:37:14 -0400

    committing change -- forget what I did

--- src/VQSD.py
+++ src/VQSD.py
@@ -219,32 +219,6 @@ class VQSD:
                 strategy=cirq.InsertStrategy.EARLIEST
             )
     
-    #def unitary(self, num_layers, params, shifted_params, copy):
-    #    """Adds the diagonalizing unitary to self.unitary_circ.
-
-    #    input:
-    #        num_layers [type: int]
-    #            number of layers to implement in the diagonalizing unitary.
-
-    #        params [type: list<list<list<float>>>]
-    #            parameters for every layer of gates
-    #            the format of params is as follows:
-
-    #            params = [[rotations for first layer],
-    #                      [rotations for second layer],
-    #                      ...,
-    #                      [rotations for last layer]]
-
-    #        shifted_params [type: list<list<list<float>>>]
-    #            parameters for the shifted layers of gates
-    #            format is the same as the format for params above
-
-    #        copy [type: int, 0 or 1, default value = 0]
-    #            the copy of the state to perform the unitary on
-    #    """
-    #    # TODO: implement
-    #    pass
-
     def layer(self, params, shifted_params, copy):
         """Implements a single layer of the diagonalizing unitary.
 
@@ -289,96 +263,15 @@ class VQSD:
             self.unitary_circ
                 appends the layer of operations to self.unitary_circ
         """
-        # for brevity
-        n = self._num_qubits
-
         if params.size != self.num_angles_required_for_layer():
             raise ValueError("incorrect number of parameters for layer")
 
-        # =====================================================================
-        # helper functions for layer
-        # =====================================================================
-
-        def gate(qubits, params):
-            """Helper function to append the two qubit gate
-            ("G" in the VQSD paper figure).
-
-            input:
-                qubits [type: list<Qubits>]
-                    qubits to be acted on. must have length 2.
-
-                params [type: list<list<angles>>]
-                    the parameters of the rotations in the gate.
-                    len(params) must be equal to 12: 4 arbitrary rotations x
-                    3 angles per arbitrary rotation.
-
-                    the format of params must be
-
-                    [[x1, y1, z1],
-                     [x2, y2, z2],
-                     [x3, y3, z3],
-                     [x4, y4, z4]].
-
-                    the general form of a gate, which acts on two qubits,
-                    is shown below:
-
-                    ----------------------------------------------------------
-                    | --Rx(x1)--Ry(y1)--Rz(z1)--@--Rx(x3)--Ry(y3)--Rz(z3)--@ |
-                G = |                           |                          | |
-                    | --Rx(x2)--Ry(y2)--Rz(z2)--X--Rx(x4)--Ry(y4)--Rz(z4)--X |
-                    ----------------------------------------------------------
-
-            modifies:
-                self.unitary_circ
-                    appends a gate acting on the qubits to the unitary circ.
-            """
-            # rotation on 'top' qubit
-            self.unitary_circ.append(
-                self._rot(qubits[0], params[0]),
-                strategy=cirq.InsertStrategy.EARLIEST
-                )
-
-            # rotation on 'bottom' qubit
-            self.unitary_circ.append(
-                self._rot(qubits[1], params[1]),
-                strategy=cirq.InsertStrategy.EARLIEST
-                )
-
-            # cnot from 'top' to 'bottom' qubit
-            self.unitary_circ.append(
-                cirq.CNOT(qubits[0], qubits[1]),
-                strategy=cirq.InsertStrategy.EARLIEST
-                )
-
-            # second rotation on 'top' qubit
-            self.unitary_circ.append(
-                self._rot(qubits[0], params[2]),
-                strategy=cirq.InsertStrategy.EARLIEST
-                )
-
-            # second rotation on 'bottom' qubit
-            self.unitary_circ.append(
-                self._rot(qubits[1], params[3]),
-                strategy=cirq.InsertStrategy.EARLIEST
-                )
-
-            # second cnot from 'top' to 'bottom' qubit
-            self.unitary_circ.append(
-                cirq.CNOT(qubits[0], qubits[1]),
-                strategy=cirq.InsertStrategy.EARLIEST
-                )
-
         # helper function for indexing loops
+        n = self._num_qubits
         stop = lambda n: n - 1 if n % 2 == 1 else n
 
         # shift in qubit indexing for different copies
         shift = 2 * self._num_qubits * copy
-
-        # =====================================================================
-        # implement the layer
-        # =====================================================================
-
-        # TODO: speedup. combine two loops into one
 
         # unshifted gates on adjacent qubit pairs
         for ii in range(0, stop(n), 2):
@@ -389,9 +282,79 @@ class VQSD:
         if n > 2:
             for ii in range(1, n, 2):
                 iiq = ii + shift
-                gate([self.qubits[iiq],
+                self._gate([self.qubits[iiq],
                       self.qubits[(iiq + 1) % n + shift]],
                      shifted_params[ii // 2])
+
+    def _gate(self, qubits, params):
+        """Helper function to append the two qubit gate
+        ("G" in the VQSD paper figure).
+
+        input:
+            qubits [type: list<Qubits>]
+                qubits to be acted on. must have length 2.
+
+            params [type: list<list<angles>>]
+                the parameters of the rotations in the gate.
+                len(params) must be equal to 12: 4 arbitrary rotations x
+                3 angles per arbitrary rotation.
+
+                the format of params must be
+
+                [[x1, y1, z1],
+                 [x2, y2, z2],
+                 [x3, y3, z3],
+                 [x4, y4, z4]].
+
+                the general form of a gate, which acts on two qubits,
+                is shown below:
+
+                ----------------------------------------------------------
+                | --Rx(x1)--Ry(y1)--Rz(z1)--@--Rx(x3)--Ry(y3)--Rz(z3)--@ |
+            G = |                           |                          | |
+                | --Rx(x2)--Ry(y2)--Rz(z2)--X--Rx(x4)--Ry(y4)--Rz(z4)--X |
+                ----------------------------------------------------------
+
+        modifies:
+            self.unitary_circ
+                appends a gate acting on the qubits to the unitary circ.
+        """
+        # rotation on 'top' qubit
+        self.unitary_circ.append(
+            self._rot(qubits[0], params[0]),
+            strategy=cirq.InsertStrategy.EARLIEST
+            )
+
+        # rotation on 'bottom' qubit
+        self.unitary_circ.append(
+            self._rot(qubits[1], params[1]),
+            strategy=cirq.InsertStrategy.EARLIEST
+            )
+
+        # cnot from 'top' to 'bottom' qubit
+        self.unitary_circ.append(
+            cirq.CNOT(qubits[0], qubits[1]),
+            strategy=cirq.InsertStrategy.EARLIEST
+            )
+
+        # second rotation on 'top' qubit
+        self.unitary_circ.append(
+            self._rot(qubits[0], params[2]),
+            strategy=cirq.InsertStrategy.EARLIEST
+            )
+
+        # second rotation on 'bottom' qubit
+        self.unitary_circ.append(
+            self._rot(qubits[1], params[3]),
+            strategy=cirq.InsertStrategy.EARLIEST
+            )
+
+        # second cnot from 'top' to 'bottom' qubit
+        self.unitary_circ.append(
+            cirq.CNOT(qubits[0], qubits[1]),
+            strategy=cirq.InsertStrategy.EARLIEST
+            )
+
 
     def _rot(self, qubit, params):
         """Helper function that returns an arbitrary rotation of the form
@@ -415,9 +378,6 @@ class VQSD:
                 appends the dip test circuit with measurements
                 on the top state.
         """
-        # TODO: implement option for partial dip test circuit
-        # or make another method (e.g., pdip_test(self, qbit_to_measure))
-
         # do the cnots
         for ii in range(self._num_qubits):
             self.dip_test_circ.append(
@@ -513,11 +473,6 @@ class VQSD:
 
     def _get_unitary_symbols(self):
         """Returns a list of symbols required for the unitary ansatz."""
-        # TODO: take into account the number of layers in the unitary
-        # this should change how num_angles_required_for_unitary() is called
-        # and the implementation of this method should change
-        # the input arguments to this method should also include the number
-        # of layers, as should num_angles_required_for_unitary()
         num_symbols_required = self.num_angles_required_for_unitary()
         return np.array(
             [sympy.Symbol(ii) for ii in range(num_symbols_required)]
@@ -531,10 +486,6 @@ class VQSD:
 
     def num_angles_required_for_unitary(self):
         """Returns the number of angles needed in the diagonalizing unitary."""
-        # TODO: take into account the number of layers.
-        # probably need to add a member variable to the class keeping track of
-        # the number of layers.
-        # should be 12 * num_qubits * num_layers
         return 12 * (self._num_qubits // 2)
 
     def num_angles_required_for_layer(self):
@@ -561,10 +512,6 @@ class VQSD:
         Returns:
             Estimate of the state overlap as a float
         """
-        # =====================================================================
-        # constants and error checking
-        # =====================================================================
-
         # number of qubits and number of repetitions of the circuit
         (nreps, nqubits) = output.shape
 
@@ -573,10 +520,6 @@ class VQSD:
 
         # initialize variable to hold the state overlap estimate
         overlap = 0.0
-
-        # =====================================================================
-        # postprocessing
-        # =====================================================================
 
         # loop over all the bitstrings produced by running the circuit
         shift = nqubits // 2
@@ -615,15 +558,10 @@ class VQSD:
 
         if angles is None:
             angles = 2 * np.random.rand(12 * self._num_qubits)
-        #param_resolver = cirq.ParamResolver(
-        #    {str(ii) : angles[ii] for ii in range(len(angles))}
-        #)
 
         return cirq.resolve_parameters(circuit, {str(ii) : angles[ii] for ii in range(len(angles))} )
-        #return circuit.with_parameters_resolved_by(param_resolver)
 
     def run(self,
-            #simulator=cirq.Simulator(),
             simulator=cirq.Simulator(),
             repetitions=1000):
         """Runs the algorithm and returns the result.
@@ -634,7 +572,6 @@ class VQSD:
 
     def run_resolved(self,
                      angles,
-                     #simulator=cirq.Simulator(),
                      simulator=cirq.Simulator(),
                      repetitions=1000):
         """Runs the resolved algorithm and returns the result."""
@@ -643,7 +580,6 @@ class VQSD:
         )
 
     def obj_dip(self,
-                #simulator=cirq.Simulator(),
                 simulator=cirq.Simulator(),
                 repetitions=1000):
         """Returns the objective function as computed by the DIP Test."""
@@ -661,7 +597,6 @@ class VQSD:
 
     def obj_dip_resolved(self,
                          angles,
-                         #simulator=cirq.Simulator(),
                          simulator=cirq.Simulator(),
                          repetitions=1000):
         """Returns the objective function of the resolved circuit as computed
@@ -679,7 +614,6 @@ class VQSD:
         return self.purity - overlap
     
     def overlap_pdip(self,
-                     #simulator=cirq.Simulator(),
                      simulator=cirq.Simulator(),
                      repetitions=1000):
         """Returns the objective function as computed by the PDIP Test."""
